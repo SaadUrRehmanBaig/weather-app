5e30dc1254b0fbbf1a78afe3f39b6bf0dfad4a20
Saad Ur Rehman <BaigSaa@corp.contoursoftware.net>
2023-01-21 20:11:48 +0500

    partial logic built with socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,19 @@ const socket = io.connect("http://localhost:3001/")
 
 function App() {
 
+  socket.on('data-client',(data)=>{
+    console.log('hello')
+  })
+
+  const get_data = (e)=>{
+    e.preventDefault()
+    console.log(123)
+    socket.emit('get_data_req','hello from get_data_req')
+  }  
+
+  socket.on('send-data-client',()=>{
+    console.log('hello from send-data-client')
+  })
   return (
     <div className="App">
       <form>
@@ -21,7 +34,7 @@ function App() {
           password:
           <input type="password" name="password" />
         </label>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" onClick={get_data}/>
       </form>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -16,8 +16,20 @@ const io = socketIo(server,{
 
 io.on('connection',(socket)=>{
     console.log("saad",socket.id)
+
+    socket.emit('data-client',/* some default data will be here*/)
+
+    socket.on('get_data_req',(data)=>{
+        console.log(data)
+        socket.emit('send-data-client',/* some default data will be here*/)
+    })
 })
 
 server.listen(3001,()=>{
     console.log('server running on port 3001')
 })
+
+
+// setInterval(() => {
+    
+// }, 300000);
