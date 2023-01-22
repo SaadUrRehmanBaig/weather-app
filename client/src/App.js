4f76ce83de0821f65c15eebd1283bc4468fd3e76
Saad Ur Rehman <BaigSaa@corp.contoursoftware.net>
2023-01-22 20:17:15 +0500

    weather app created taking asssumption that different screens are different users

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,108 @@
 import './App.css';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import io from "socket.io-client"
 
 const socket = io.connect("http://localhost:3001/")
 
+
+
 function App() {
 
-  socket.on('data-client',(data)=>{
-    console.log(data)
-  })
+  const [info, setInfo] = useState([])
+  const [city, setCity] = useState("")
+  const [temp,setTemp] = useState(true)
 
-  socket.on('send-data-client',()=>{
-    console.log('hello from send-data-client')
-  })
+  useEffect(() => {
+    socket.on('error',()=>{
+      alert('enter correct city')
+    })
+    socket.on('data-client', (data) => {
+      setInfo(data)
+      console.log(data)
+    })
 
-  const get_data = (e)=>{
-    e.preventDefault()
-    console.log(123)
-    socket.emit('get_data_req',city)
-  }  
+    socket.on('send-data-client', (data) => {
+      setInfo(data)
+      console.log(data)
+    })
 
-  const [city,setCity]=useState("")
+    socket.on('data updated', () => {
+      console.log('on data updated from client')
+      socket.emit('data-client')
+    })
+
+    return ()=>{
+      socket.disconnect()
+      socket.Close();
+    }
+  }, [socket])
+
+  const get_data = () => {
+    socket.emit('get_data_req', city.toLowerCase())
+  }
+
+  const del = (city_data)=>{
+    socket.emit('delete',city_data)
+    console.log(city_data)
+  }
+
   return (
     <div className="App">
-      <form>
-        <label>
-          city:
-          <input type="text" name="city" onChange={(e)=>{setCity(e.target.value)}} />
-        </label>
-        <label>
-          email:
-          <input type="email" name="email" />
-        </label>
-        <label>
-          password:
-          <input type="password" name="password" />
-        </label>
-        <input type="submit" value="Submit" onClick={get_data}/>
-      </form>
+      <div style={{ height: '100%' }}>
+        <div className='text-center pt-4'>
+          <label>city:
+            <input className="card text-dark border border-info rounded-3 p-2 mx-2" type="search" name="city" onChange={(e) => { setCity(e.target.value) }} />
+          </label>
+          <button className='btn btn-outline-info text-center ml-4 ' onClick={get_data}>Submit</button>
+
+          <button className='btn btn-outline-info text-center ml-4 text-dark  bg-warning tempbtn'  onClick={()=>{setTemp(!temp)}}>{temp?'°C':'°F'}</button>
+        </div>
+        
+        <div className='container  pt-5'>
+          <div className='row'>
+            {temp?(<>
+              {Object.keys(info).map((city_data) => {
+              let city_data_details = info[city_data]
+              return (<div className='col-12 col-md-4 my-4' key={city_data}>
+                <div className="card text-dark border border-info rounded-3 " style={{ width: '18rem', }}>
+                  <div className="card-body">
+                    <img src={city_data_details.current.condition.icon}/>
+                    <h5 className="card-text"><b>{city_data_details.location.name}</b> </h5>
+                    <p className="card-text"><b>{city_data_details.current.condition.text}</b> </p>
+                    <p className="card-text"><b>Tempreture {city_data_details.current.temp_c} °C</b> </p>
+                    <p className="card-text"><b>Humidity {city_data_details.current.humidity}% </b></p>
+                    <p className="card-text"><b> {city_data_details.location.region}, {city_data_details.location.country}</b> </p>
+                    <div>
+                      <button className='btn-outline-danger btn mx-1' onClick={()=>{del(city_data)}}>Delete</button>
+                    </div>
+                  </div>
+                </div>
+              </div>)
+            })}
+            </>):(<>
+              {Object.keys(info).map((city_data) => {
+              let city_data_details = info[city_data]
+              return (<div className='col-12 col-md-4 my-4' key={city_data}>
+                <div className="card text-dark border border-info rounded-3 " style={{ width: '18rem', }}>
+                  <div className="card-body">
+                    <img src={city_data_details.current.condition.icon}/>
+                    <h5 className="card-text"><b>{city_data_details.location.name}</b> </h5>
+                    <p className="card-text"><b>{city_data_details.current.condition.text}</b> </p>
+                    <p className="card-text"><b>Tempreture {city_data_details.current.temp_f} °F</b> </p>
+                    <p className="card-text"><b>Humidity {city_data_details.current.humidity}% </b></p>
+                    <p className="card-text"><b> {city_data_details.location.region}, {city_data_details.location.country}</b> </p>
+                    <div>
+                      <button className='btn-outline-danger btn mx-1' onClick={()=>{del(city_data)}}>Delete</button>
+                    </div>
+                  </div>
+                </div>
+              </div>)
+            })}
+            </>)}
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -9,49 +9,82 @@ const axios = require('axios')
 const app = express()
 app.use(cors())
 
-const default_cities = ['karachi','lahore','islamabad','peshawar','gwadar']
-const city_data =  []
+const default_cities = ['karachi', 'lahore', 'islamabad', 'peshawar', 'gwadar'];
+const cities_data = {}
 
-async function get_data(city){
-    if (typeof(city) === 'string'){
-        const address = `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${city}`;
-        const data = await axios.get(address)
-        city_data.push(data.data)
+async function get_data(city,socket={}) {
+    if (typeof (city) === 'string') {
+        try {
+            const address = `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${city}`;
+            const data = await axios.get(address)
+            cities_data[city] = (data.data)
+        }catch(e){
+            socket.emit('error',{})
+            console.log(e)
+        }
     }
 }
 
-default_cities.map((city)=>{get_data(city)})
+default_cities.map((city) => { get_data(city) })
+
+setInterval(async () => {
+    await default_cities.map((city) => { get_data(city) });
+    io.local.emit('data updated')
+}, 600000)
+
 
 const server = http.createServer(app)
-const io = socketIo(server,{ 
+const io = socketIo(server, {
     cors: {
-      origin: 'http://localhost:3000'
+        origin: 'http://localhost:3000'
     }
 })
 
-io.on('connection',(socket)=>{
-    const local_arr = [...default_cities]
+io.on('connection', (socket) => {
 
-    console.log("connection established",socket.id)
+    let start = 0;
+    let end = 5;
+    let local_arr = [...default_cities.slice(start, end)]
+    let local_data = {}
+
+    local_arr.forEach((city) => {
+        local_data[city] = cities_data[city]
+    })
+
+    console.log("connection established", socket.id)
     /* associate user id from socket id here*/
 
-    socket.emit('data-client',city_data)
+    socket.emit('data-client', local_data)
 
-    socket.on('get_data_req',(data)=>{
-        local_arr.push(data)
-        console.log(socket.id,local_arr)
-        socket.emit('send-data-client',/* some data will be here*/)
+    socket.on('data-client', () => {
+        local_arr.forEach((city) => {
+            local_data[city] = cities_data[city]
+        })
+        socket.emit('data-client', local_data)
     })
 
-    socket.on('disconnect',()=>{
-        console.log('disconnected')
-        /* disassociate user id from socket id here*/
+    socket.on('get_data_req', async (city) => {
+
+        if (!local_arr.includes(city) && !default_cities.includes(city)) {
+            local_arr.push(city)
+            default_cities.push(city)
+            await get_data(city,socket)
+        }
+        local_data[city] = cities_data[city]
+        socket.emit('send-data-client', local_data)
     })
+
+    socket.on('delete',(data)=>{
+        if (data in local_data){
+            delete local_data[data]
+            local_arr.splice(local_arr.indexOf(data),1)
+            socket.emit('send-data-client', local_data)
+        }
+    })
+
 })
 
-
-
-server.listen(3001,()=>{
+server.listen(3001, () => {
     console.log('server running on port 3001')
 })
 
